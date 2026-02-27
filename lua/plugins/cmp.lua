b93691f764a991e8b3d4a7528b5f1ddb551d982a
AokiMasataka <55342755+AokiMasataka@users.noreply.github.com>
2026-02-28 00:23:24 +0900

    fix: recomend

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,19 +1,24 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local cmp = require("cmp")
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+  },
+  config = function()
+    local cmp = require("cmp")
 
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-        },
-      })
-    end,
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+      }, {
+        { name = "buffer", keyword_length = 2 },
+        { name = "path" },
+      }),
+    })
+  end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,42 +1,10 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
     config = function()
-      vim.lsp.config("pyright", {})
-      vim.lsp.enable("pyright")
-
-      vim.lsp.config("rust_analyzer", {})
-      vim.lsp.enable("rust_analyzer")
-
-      -- LSP 共通キーマップ（attach 時のみ）
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local opts = { buffer = args.buf, silent = true }
-          
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-
-          -- 定義・参照
-          vim.keymap.set("n", "cd", vim.lsp.buf.definition, opts)
-          vim.keymap.set("n", "cr", vim.lsp.buf.references, opts)
-          vim.keymap.set("n", "ci", vim.lsp.buf.implementation, opts)
-
-          -- 情報表示
-          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-          vim.keymap.set("n", "cp", function()
-            local params = vim.lsp.util.make_position_params()
-            vim.lsp.buf_request(0, "textDocument/definition", params, function(_, result)
-              if result and result[1] then
-                vim.lsp.util.preview_location(result[1])
-              end
-            end)
-          end,
-          { desc = "Preview definition" }
-        )
-        end,
-      })
-
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
@@ -44,10 +12,32 @@ return {
         capabilities = capabilities,
       })
       vim.lsp.enable("pyright")
-      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- require("lspconfig").pyright.setup({
-      --   capabilities = capabilities,
-      -- })
+
+      vim.lsp.config("rust_analyzer", {
+        capabilities = capabilities,
+      })
+      vim.lsp.enable("rust_analyzer")
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local opts = { buffer = args.buf, silent = true }
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          client.server_capabilities.semanticTokensProvider = nil
+
+          vim.keymap.set("n", "cd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "cr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "ci", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "cp", function()
+            local params = vim.lsp.util.make_position_params()
+            vim.lsp.buf_request(0, "textDocument/definition", params, function(_, result)
+              if result and result[1] then
+                vim.lsp.util.preview_location(result[1])
+              end
+            end)
+          end, { desc = "Preview definition" })
+        end,
+      })
     end,
   },
 }
